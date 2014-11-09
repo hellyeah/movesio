f11504c266872894fb9c3d6e51ec9db993b0e769
gailees <gailees@umich.edu>
2014-11-09 00:51:12 -0800

    bower installed angular and npm installed grunt-wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,9 @@
 
 module.exports = function (grunt) {
 
+  //load wiredep
+  grunt.loadNpmTasks('grunt-wiredep');
+
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
@@ -59,6 +62,26 @@ module.exports = function (grunt) {
           '<%= config.app %>/manifest.json',
           '<%= config.app %>/_locales/{,*/}*.json'
         ]
+      }
+    },
+
+    wiredep: {
+      task: {
+        // Point to the files that should be updated when
+        // you run `grunt wiredep`
+        src: [
+          'app/views/*.html',   // .html support...
+          'app/views/**/*.jade',   // .jade support...
+          'app/styles/main.scss',  // .scss & .sass support...
+          'app/config.yml'         // and .yml & .yaml support out of the box!
+        ],
+
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+        }
       }
     },
 
